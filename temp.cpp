23b67576b0f1553c6af5aeb892d780288e23f813
woorimlee <and_lamyland@naver.com>
2019-07-14 17:21:19 +0900

    temp

--- temp.cpp
+++ temp.cpp
@@ -1,29 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-void find(int n);
+#include<time.h>
 
-int main() {
+int main()
+{
 	int n;
-	printf("양의 정수 n을 입력하세요\n");
-	scanf("%d", &n);
-	find(n);
+	int arr[10];
+	int seed;
+	scanf_s("%d %d", &n, &seed);
+	srand(seed);	//seed값 사용
+
+	for (int i = 0; i < 10; i++) {
+		arr[i] = rand() % n;
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
 	return 0;
 }
-void find(int n) {
-	for (int i = 0; i < n; i++) {
-		if (i == 0 || i == n - 1) {
-			for (int j = 1; j <= n; j++) {
-				printf("*");
-			}
-			printf("\n");
-		}
-		else {
-			for (int j = n - i; j > 1; j--) {//j는 n-i부터 2까지 띄어쓰기를 합니다.
-				printf(" ");
-			}
-			printf("*");//가운데의 마지막 부분에 *를 출력합니다.
-			printf("\n");
-		}
-	}
-}
+
+
 
