fe3a8ee0413a9736aa5c632f05b54f354c5b87fc
woorimlee <and_lamyland@naver.com>
2020-02-05 22:00:38 +0900

    아직

---
+++ baek/9935_문자열폭발.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+	string fir;
+	string sec;
+	string ans;
+	cin >> fir >> sec;
+	
+	for (int i = 0; i < fir.size(); i++) {
+		// sec에 있는 단어? 
+		// 다음 단어 검사
+		// sec에 없는 단어?
+		// ans에 추가
+	}
+
+
+	return 0;
+}
