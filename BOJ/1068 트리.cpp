a0a464d3522f95918e87769439a3f9015d0831ec
woorimlee <and_lamyland@naver.com>
2020-02-25 17:12:51 +0900

    주석에 설명 있음

--- BOJ/1068 트리.cpp
+++ BOJ/1068 트리.cpp
@@ -4,16 +4,30 @@
 using namespace std;
 
 int N, delete_node, root, reaf=0;
+//트리를 저장할 이차원 벡터
 vector <vector <int> > graph;
 bool visited[51] = { false };
 
-bool dfs(int node) {
-	if 
-
+void dfs(int node) {
+	//dfs 들어온 노드는 검사했다고 처리함
+	visited[node] = true;
+	//base case
+	if (node >= N) {
+		return;
+	}
+	//자식 노드의 존재를 확인
+	bool child = false;
 	for (int i = 0; i < graph[node].size(); i++) {
 		if (visited[graph[node][i]] == false) {
-			if()
+			//자식 노드가 있는 상태 && 자식 노드 방문하지 않은 상태
+			child = true;
+			dfs(graph[node][i]);
 		}
+	}
+	//자식 노드가 없다면
+	if (!child) {
+		//답 1 증가
+		reaf++;
 	}
 }
 int main() {
@@ -33,8 +47,11 @@ int main() {
 		}
 	}
 	cin >> delete_node;
+	//지울 노드는 dfs로 탐색하지 않을 예정
 	visited[delete_node] = true;
-	dfs(root);
+	//루트 노드를 지울 노드로 선정하는 경우가 있다고 함.
+	if(visited[root] == false) 
+		dfs(root);
 	cout << reaf;
 	
 	return 0;
