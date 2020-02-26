1e6c84c7b3f3574d689e95557c09df0da24f1581
woorimlee <and_lamyland@naver.com>
2020-02-26 19:55:25 +0900

    주석에 설명 추가

--- BOJ/11724 연결 요소의 개수.cpp
+++ BOJ/11724 연결 요소의 개수.cpp
@@ -15,12 +15,14 @@ int main() {
 	int nodeA, nodeB;
 	for (int i = 0; i < M; i++) {
 		cin >> nodeA >> nodeB;
+		//무방향 그래프를 만들어줌
 		vec[nodeA].push_back(nodeB);
 		vec[nodeB].push_back(nodeA);
 	}
 
 	for (int i = 1; i <= N; i++) {
-		//방문한 노드가 아니라면
+		//모든 노드를 기준으로 이미 방문한 노드가 아니라면 
+		//dfs 탐색 시작
 		if (!visited[i]) {
 			dfs(i);
 			ans++;
@@ -31,6 +33,7 @@ int main() {
 }
 
 void dfs(int node) {
+	//연결된 모든 노드를 dfs로 탐색할 것임
 	visited[node] = true;
 	for (int i = 0; i < vec[node].size(); i++) {
 		//방문한 노드가 아니라면
