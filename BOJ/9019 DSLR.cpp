0a1bb304dbd08a3e5e8d172a5b25471c04437b44
woorimlee <and_lamyland@naver.com>
2020-02-29 15:26:46 +0900

    주석에 설명 추가

--- BOJ/9019 DSLR.cpp
+++ BOJ/9019 DSLR.cpp
@@ -1,85 +1,70 @@
 #include <iostream>
 #include <queue> 
-#include <memory>
+#include <memory.h>
+#include <string>
 
 using namespace std;
-
-int print_DSLR(int A, int now, char * operation, int * path) {
-	if (now == A) {
-		return 0;
-	}
-	int n = 0;
-	n = path[now];
-
-	print_DSLR(A, n, operation, path);
-	cout << operation[now];
-	return 0;
-}
 
 int main(void) {
 
 	int T;
-	cin >> T; 
-	char operation[10000];
-	bool check[10000] = { false, };
-	int path[10000] = { 0, };
+	cin >> T;
+	bool check[10001] = { false, };
+	int A, B;
 
 	while (T--) {
-		//1. bfs 저장할 queue
-		//2. 검사한 queue인지 저장할 배열
-		//3. 이동 숫자 경로를 저장한 int 배열과
-		//4. 그 숫자경로 별 적용시킨 연산을 저장할 char 배열
-		queue <int> q;
-		
-		int A, B, now, next;
+		//dslr에는 사용된 operator와 적용된 결괏값을 저장할 것임.
+		queue <pair<string, int> > dslr;
+		pair <string, int> now;
+		int next;
 		cin >> A >> B;
 		memset(check, false, sizeof(check));
+		dslr.push({ "", A });
+		check[A] = true;
 		
-		q.push(A); 
-		while (!q.empty()) {
-			now = q.front(); q.pop();
-			if (now == B) {
+		while (!dslr.empty()) {
+			now = dslr.front(); 
+			dslr.pop();
+
+			//정답이면 반복 끝
+			if (now.second == B) {
 				break;
 			}
 
-			next = (now * 2) % 10000;
+			//D 실행결과 반영
+			next = (now.second * 2) % 10000;
 			if (check[next] == false) {
-				q.push(next);
+				//pair의 string 부분에는 현재 + D를 더한 값을 넘기고
+				//pair의 int 부분에는 next 값을 넘겨
+				dslr.push({ now.first + 'D', next });
 				check[next] = true;
-				path[next] = now;
-				operation[next] = 'D';
 			}
 
-			next = (now - 1);
-			if (next == 0) next = 9999;
+			//S 실행결과 반영
+			//현재 값 -1이 0이면 9999를 저장하는게 아님!
+			//현재 값 == 0이면 9999를 저장, 아니면 -1 값 저장
+			next = ((now.second) == 0) ? 9999 : now.second - 1;
 			if (check[next] == false) {
-				q.push(next);
+				dslr.push({ now.first + 'S', next });
 				check[next] = true;
-				path[next] = now;
-				operation[next] = 'S';
 			}
 
-			next = ((now % 1000) * 10) + (now / 1000);
+			//L 실행결과 반영
+			next = ((now.second % 1000) * 10) + (now.second / 1000);
 			if (check[next] == false) {
-				q.push(next);
+				dslr.push({ now.first + 'L', next });
 				check[next] = true;
-				path[next] = now;
-				operation[next] = 'L';
 			}
 
-			next = ((now % 10) * 1000) + (now / 10);
+			//R 실행결과 반영
+			next = ((now.second % 10) * 1000) + (now.second / 10);
 			if (check[next] == false) {
-				q.push(next);
+				dslr.push({ now.first + 'R', next });
 				check[next] = true;
-				path[next] = now;
-				operation[next] = 'R';
 			}
 		}
-		print_DSLR(A, now, operation, path);
-		cout << endl;
+		cout << now.first << "\n";
 	}
-
-
 
 	return 0;
 }
