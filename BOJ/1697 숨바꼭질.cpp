66d8ca5451c9c8187475b2d7ada558c063ce0ac6
woorimlee <and_lamyland@naver.com>
2020-03-20 17:00:37 +0900

    코드를 좀 더 이쁘게 바꿈. 주석에 설명 있음

--- BOJ/1697 숨바꼭질.cpp
+++ BOJ/1697 숨바꼭질.cpp
@@ -29,7 +29,52 @@ int main(void) {
 		//1) 현재위치 -1
 		//2) 현재위치 +1
 		//3) 현재위치 *2
-		
+		int calc[3] = { cp.first - 1, cp.first + 1, cp.first * 2 };
+		for (int i = 0; i < 3; i++) {
+			if (0 <= calc[i] && calc[i] < 100001) {
+				if (!visited[calc[i]]) {
+					sb.push({ calc[i], cp.second + 1 });
+					visited[calc[i]] = true;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+/* 아래의 코드를 for구문과 배열을 써서 위와 같이 바꿈
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int N, K;
+bool visited[100001] = { false };
+//수빈이의 위치 값, 걸린 시간이 들어있음
+queue <pair<int, int> > sb;
+
+int main(void) {
+
+	cin >> N >> K;
+
+	sb.push({ N, 0 });
+	visited[N] = true;
+	pair <int, int> cp; //current position
+	while (!sb.empty()) {
+		cp = sb.front();
+		sb.pop();
+
+		//수빈이의 위치와 동생의 위치가 같다면
+		if (cp.first == K) {
+			cout << cp.second;
+			return 0;
+		}
+
+		//bfs로 탐색할 종류
+		//1) 현재위치 -1
+		//2) 현재위치 +1
+		//3) 현재위치 *2
+
 		//1)
 		//현재 위치 - 1이 0보다는 커야함
 		if (cp.first - 1 >= 0) {
@@ -38,7 +83,7 @@ int main(void) {
 				visited[cp.first - 1] = true;
 			}
 		}
-		
+
 		//2)
 		if (cp.first + 1 < 100001) {
 			if (!visited[cp.first + 1]) {
@@ -58,3 +103,4 @@ int main(void) {
 	}
 	return 0;
 }
+*/
