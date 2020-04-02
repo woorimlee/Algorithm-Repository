8aeec37555cb97c1c35f2185c541a3838c2a19d3
woorimlee <and_lamyland@naver.com>
2020-04-02 21:24:54 +0900

    푸는 중

--- BOJ/3988 수 고르기.cpp
+++ BOJ/3988 수 고르기.cpp
@@ -1,85 +1,47 @@
-#include <iostream>
-
+/*#include <iostream>
 #include <deque>
-
 #include <algorithm>
 
 using namespace std;
-
-
-
 const int MAX = 1000000;
-
 const int INF = 987654321;
-
-
 
 int arr[MAX];
 
-
-
 int main(void)
-
 {
-
-	ios_base::sync_with_stdio(0);
-
-	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
 	int N, K;
-
 	cin >> N >> K;
 
-
-
 	for (int i = 0; i < N; i++)
-
 		cin >> arr[i];
-
-
 
 	sort(arr, arr + N);
 
 	deque<int> dq; //idx
-
 	int result = INF;
 
-
-
 	K = N - K;
-
 	//슬라이딩 윈도우
-
 	for (int i = 0, j = 1; i + K <= N; i++)
-
 	{
-
 		if (!dq.empty() && dq.front() <= i)
-
 			dq.pop_front();
 
-
-
 		//K개씩 확인
-
 		for (; j < i + K; j++)
-
 		{
-
 			//arr[j] - arr[j - 1]이 최대여야한다
-
 			while (!dq.empty() && arr[j] - arr[j - 1] <= arr[dq.back()] - arr[dq.back() - 1])
-
 				dq.pop_back();
 
 			dq.push_back(j);
-
 		}
 
-
-
 		int m = arr[dq.front()] - arr[dq.front() - 1]; //맨 앞 두 개의 차
-
 		int M = arr[i + K - 1] - arr[i]; //양 끝 차
 		cout << " m & M : " << m << " " << M << " : ";
 		for (auto it : dq) {
@@ -87,20 +49,8 @@ int main(void)
 		}cout << "\n";
 
 		result = min(result, m + M);
-
 	}
 
-	cout << result << "\n";
-
+	cout <<result << "\n";
 	return 0;
-
-}
-
-/*
-6 2
--5 8 10 1 13 -1
-5 2
--3 -2 2 6 8
-5 2
-0 1 2 9999 10000
-*/
+}*/
