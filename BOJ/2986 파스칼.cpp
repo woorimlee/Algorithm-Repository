615fda359233d6213750d0957bc26df72973bc16
woorimlee <and_lamyland@naver.com>
2020-04-03 12:45:08 +0900

    주석으로 풀이 설명 추가

--- BOJ/2986 파스칼.cpp
+++ BOJ/2986 파스칼.cpp
@@ -1,29 +1,20 @@
 #include <iostream>
-
-bool prime[1000000001] = { false, };
-
+#include <cmath>
 using namespace std;
 
 int main() {
 	int N;
 	cin >> N;
-
-	for (int i = 2; i * i <= 1000000001; i++) {
-		if (prime[i] == false)
-			for (int j = i * i; j <= 1000000001; j += i) {
-				prime[j] = true;
-			}
-	}
-	if (prime[N]) {
-		cout << N - 1;
-	}
-	else {
-		for (int i = N / 2; i >= 1; i--) {
-			if (N % i == 0) {
-				cout << N - i;
-			}
+	//N이 소수인지 아닌지 찾을 때, 
+	//x는 2부터 시작해서 x의 제곱이 N을 초과하는 순간
+	//그 수는 소수라고 판단함.
+	//즉, 소수가 아니라면 x=2부터 시작해서 x의 제곱이 N 이하일 때까지만 검사하면 약수는 나옴.
+	for (int i = 2; i*i <= N; i++) {
+		if (N % i == 0) {
+			cout << (N - (N/i));
+			return 0;
 		}
-
 	}
+	cout << N - 1;
 	return 0;
 }
