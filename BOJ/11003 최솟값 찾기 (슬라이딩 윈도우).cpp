56d0201581643e0aca382326a7e825c7dfe11651
woorimlee <and_lamyland@naver.com>
2020-04-05 14:39:04 +0900

    주석에 자세한 설명 추가

--- BOJ/11003 최솟값 찾기 (슬라이딩 윈도우).cpp
+++ BOJ/11003 최솟값 찾기 (슬라이딩 윈도우).cpp
@@ -1,5 +1,6 @@
-/*#include <iostream>
-#include <queue>
+#include <iostream>
+#include <vector>
+#include <deque>
 
 using namespace std;
 
@@ -10,6 +11,9 @@ void solve_2();
 int find_min(deque <int>& dq);
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	cin >> N >> L;
 
 	//슬라이딩 윈도우 
@@ -60,7 +64,60 @@ int find_min(deque <int> &dq) {
 }
 
 void solve_2() {
+	//Deque(Double ended queue)을 이용한 기법으로,
+	//주어진 배열에 슬라이딩 윈도우를 적용시켜서 해당 윈도우내에 있는 최솟값을 덱의 front에 둘 예정.
+	vector <int> arr(N);
+	int i;
+	for (i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
 
+	//덱에는 arr 배열의 슬라이딩 인덱스가 적용된 부분에서
+	//최솟값과 관계된 부분의 인덱스를 저장할 것임.
+	deque <int> dq;
 
+	/*
+	//첫 번째 슬라이딩 윈도우를 만들기까지 처리
+	for (i = 0; i < L; i++) {
+		//dq의 front에는 가장 작은 값이 들어있을 것임.
+		//dq의 back에는 작은 값 후보들(윈도우가 슬라이딩되며 front가 인덱스 초과시 pop 됨)
+		//이 있는데, 그 후보보다 새로 검사하는 arr[i] 값이 더 작으면
+		//후보 갱신
+		while ((!dq.empty()) && arr[dq.back()] > arr[i]) {
+			dq.pop_back();
+		}
 
-}*/
+		dq.push_back(i);
+		cout << arr[dq.front()] << " ";
+	}
+
+	//k개의 배열을 바라보는 슬라이딩 윈도우가 생성된 이후임.
+	//한 칸씩 밀어가면서 최솟값 찾기
+	for (; i < N; i++) {
+		//인덱스가 윈도우 넘어갔으면 pop
+		while ((!dq.empty()) && dq.front() <= i - L) {
+			dq.pop_front();
+		}
+		while ((!dq.empty()) && arr[dq.back()] > arr[i]) {
+			dq.pop_back();
+		}
+		dq.push_back(i);
+		cout << arr[dq.front()] << " ";
+	}*/
+
+	//보통은 위 두 개로 나눠야하는 데,
+	//이 문제는 하나로 합쳐도 상관없어서 아래처럼 함.
+	for (i = 0; i < N; i++) {
+		//인덱스가 윈도우 넘어갔으면 pop
+		while ((!dq.empty()) && dq.front() <= i - L) {
+			dq.pop_front();
+		}
+		while ((!dq.empty()) && arr[dq.back()] > arr[i]) {
+			dq.pop_back();
+		}
+		dq.push_back(i);
+		cout << arr[dq.front()] << " ";
+	}
+
+	return;
+}
