7434b6aede89a91993d2c9d9d2a0ff92b81191e9
woorimlee <and_lamyland@naver.com>
2020-04-10 19:11:21 +0900

    작성중

--- 선형 시간 선택 알고리즘.cpp
+++ 선형 시간 선택 알고리즘.cpp
@@ -40,7 +40,7 @@ int GetRandomInt(int min, int max) {
 }
 
 int LinearTimeSelection(array <int, ARRSIZE> & arr, int s, int e, int k);
-int WorstCaseLinearTimeSelection();
+int WorstCaseLinearTimeSelection(array <int, ARRSIZE>& arr, int start, int end, int k);
 
 int main() {
 	array <int, ARRSIZE> arr;
@@ -54,11 +54,14 @@ int main() {
 	}
 
 	int k = GetRandomInt(1, 11);
-	cout << "\n" << k << "번째 작은 원소의 값 : ";
-	/*array <int, 10> test = { 5, 2, 3, 4, 6, 7, 8, 1, 11, 10 };
-	int test_k = 9;
+	cout << "\n" << k << "번째 작은 원소의 값 :\n";
+	cout << "1. 평균 선형시간 : ";
+	/*array <int, 10> test = { 1, 2, 3, 4, 6, 7, 8, 9, 10, 11 };
+	int test_k = 1;
 	cout << LinearTimeSelection(test, 0, 9, test_k);*/
 	cout << LinearTimeSelection(arr, 0, arr.size() - 1, k) << "\n";
+	cout << "2. 최악의 경우 선형 시간 : ";
+	cout << WorstCaseLinearTimeSelection(arr, 0, arr.size() - 1, k) << "\n";
 	cout << "정렬된 배열 : ";
 	sort(arr.begin(), arr.end());
 	for (auto it : arr) {
@@ -87,6 +90,7 @@ int LinearTimeSelection(array <int, ARRSIZE> & arr, int start, int end, int k) {
 		return arr[start];
 	}
 
+	//partition
 	//피봇이 몇 번째 위친지 구하기
 	int i = start; //교체할 인덱스
 	int j = start; //비교할 인덱스
@@ -101,10 +105,11 @@ int LinearTimeSelection(array <int, ARRSIZE> & arr, int start, int end, int k) {
 	//위 반복문에서 마지막에 i++하기 때문에 피봇의 인덱스 값이 1 증가 된 상태.
 
 	//확인용
-	//for (auto it : arr) {
-	//	cout << it << " ";
-	//}cout << "\n";
-	//cout << start  <<" " << end << " "  << i<< "\n";
+	/*
+	for (auto it : arr) {
+		cout << it << " ";
+	}cout << "\n";
+	cout << start  <<" " << end << " "  << i<< "\n";*/
 
 	
 	//찾는 위치 == 피봇 위치면 답 찾음
@@ -121,6 +126,36 @@ int LinearTimeSelection(array <int, ARRSIZE> & arr, int start, int end, int k) {
 	}
 }
 
-int WorstCaseLinearTimeSelection() {
+int WorstCaseLinearTimeSelection(array <int, ARRSIZE>& arr, int start, int end, int k) {
+	/*
+	알고리즘
+	1. 전체 배열을 5개씩 나눠 총 n/5개의 그룹을 만든다.
+	2. 각 그룹을 정렬하고 중간 값을 찾아낸다. 이때, 각 그룹의 중간 값을 m1, m2, ... mn/5로 명명한다.
+	3. m1, m2, ... mn/5의 값 중 중간 값을 본 함수의 재귀함수를 통해 구한다. (k = 배열 크기 / 2로 넘김)
+	4. 3에서 구한 중간 값을 M이라고 할 때, M을 배열의 맨 뒤로 보내고 퀵 소트 방식을 이용해 
+		M보다 작은 값은 배열의 왼쪽으로, 큰 값은 오른쪽으로 분할한다.
+	5. k == M의 위치라면 M의 값을, K > M의 위치라면 오른쪽 배열을, K < M의 위치라면 왼쪽 배열을 탐색한다.
+	*/
+
+	//처음과 끝이 같으면 리턴
+	if (start == end) {
+		return arr[start];
+	}
+	//5개 이하이면 찾아보고 리턴
+	else if (end <= 4) {
+		/*for (int i = 0; i < end; i++) {
+			if()
+		}*/
+	}
+
+	//n/5개의 그룹으로 나누고 중간 값을 저장한다.
+
+	//m1, m2, ..., mn/5 의 중간 값을 재귀적으로 구한다.
+
+	//M을 pivot으로 M의 index를 구한다.
+
+	
+	//M의 인덱스와 k를 비교하고 subarray에 대해 재귀 탐색을 실행한다.
+
 	return 0;
 }
