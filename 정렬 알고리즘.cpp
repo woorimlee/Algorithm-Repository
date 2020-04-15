6f5d18f498a71fc1679cb8e23254d9cdb8186f29
woorimlee <and_lamyland@naver.com>
2020-04-15 21:11:29 +0900

    퀵 정렬 추가

--- 정렬 알고리즘.cpp
+++ 정렬 알고리즘.cpp
@@ -3,6 +3,8 @@
 #include <array>
 #include <algorithm>
 
+#define ARRSIZE 10
+
 using namespace std;
 
 //난수 생성함수
@@ -22,44 +24,50 @@ int GetRandomInt(int min, int max) {
 	return range(mt);
 }
 
-void SelectionSort(array<int, 10> & arr);
-void BubbleSort(array<int, 10> & arr);
-void ImprovedBubbleSort(array<int, 10> & arr);
-void InsertionSort(array<int, 10> & arr);
+void SelectionSort(array<int, ARRSIZE> & arr);
+void BubbleSort(array<int, ARRSIZE> & arr);
+void ImprovedBubbleSort(array<int, ARRSIZE> & arr);
+void InsertionSort(array<int, ARRSIZE> & arr);
 //합병 정렬용 아래 세 개의 함수
-void MergeSort(array<int, 10> & arr);
-void RunMS(array <int, 10> & arr, int left, int right);
-void Merge(array <int, 10> & arr, int left, int mid, int right);
+void MergeSort(array<int, ARRSIZE> & arr);
+void RunMS(array <int, ARRSIZE> & arr, int left, int right);
+void Merge(array <int, ARRSIZE> & arr, int left, int mid, int right);
+void QuickSort(array<int, ARRSIZE> & arr, int left, int right);
 
 int main() {
-	array <int, 10> arr = { 0 };
-	for (int i = 0; i < 10; i++) {
-		arr[i] = GetRandomInt(-20, 20);
+	for (int i = 0; i < 100; i++) {
+		array <int, ARRSIZE> arr = { 0 };
+		for (int i = 0; i < ARRSIZE; i++) {
+			arr[i] = GetRandomInt(-5, 5);
+		}
+		for (auto it : arr) {
+			cout << it << " ";
+		}
+		cout << "==> 정렬 전 배열\n======================================\n";
+
+		//이하 알고리즘 평균 O(N^2)
+		//SelectionSort(arr);
+		//BubbleSort(arr);
+		//ImprovedBubbleSort(arr);
+		//InsertionSort(arr);
+		//이하 알고리즘 평균 O(NlogN)
+		//MergeSort(arr);
+		QuickSort(arr, 0, ARRSIZE - 1);
+		for (auto it : arr) {
+			cout << it << " ";
+		}cout << "\n";
+
+
+		cout << "==> 정렬 후 배열\n";
 	}
-	for (auto it : arr) {
-		cout << it << " ";
-	}
-	cout << "==> 정렬 전 배열\n======================================\n";
-	
-	//이하 알고리즘 평균 O(N^2)
-	//SelectionSort(arr);
-	//BubbleSort(arr);
-	//ImprovedBubbleSort(arr);
-	//InsertionSort(arr);
-	MergeSort(arr);
-
-	//이하 알고리즘 평균 O(NlogN)
-
-
-	cout << "==> 정렬 후 배열\n";
 	return 0;
 }
 
 //선택 정렬. 최대 혹은 최솟값을 찾아서 맨 뒤나 맨 앞으로 옮긴다.
-void SelectionSort(array<int, 10> & arr) {
+void SelectionSort(array<int, ARRSIZE> & arr) {
 	//O(N^2)
 	int index = 0;
-	array<int, 10> sorted_arr = arr;
+	array<int, ARRSIZE> sorted_arr = arr;
 	//1. 배열에서 가장 큰 값을 찾는다.
 	//2. 맨 뒤의 값이랑 swap한다. (swap된 맨 뒷 부분 다음 검색에서 제외)
 	//3. 맨 앞의 자료는 최솟값으로 정렬되니, i가 1이 될때까지만 위 과정을 반복한다.
@@ -81,9 +89,9 @@ void SelectionSort(array<int, 10> & arr) {
 }
 
 //버블 정렬. 이웃한 두 원소를 비교해 정렬이 필요하면 swap.
-void BubbleSort(array<int, 10> & arr) {
+void BubbleSort(array<int, ARRSIZE> & arr) {
 	//O(N^2)
-	array<int, 10> sorted_arr = arr;
+	array<int, ARRSIZE> sorted_arr = arr;
 	for (int i = 0; i < sorted_arr.size() - 1; i++) {
 		//두 개씩 비교해나가니까 맨 마지막 원소의 위치 -1 까지만 for 구문을 돌림.
 		//+ 정렬 한 번 끝나면 맨 끝에는 검사할 필요가 없음
@@ -101,7 +109,7 @@ void BubbleSort(array<int, 10> & arr) {
 }
 
 //Improved Bubble Sort. Modified Bubble Sort.
-void ImprovedBubbleSort(array<int, 10> & arr) {
+void ImprovedBubbleSort(array<int, ARRSIZE> & arr) {
 	/*버블 정렬을 한 창 시행중인데, 더이상 정렬이 필요없는 상황이 있을 수 있음.
 	ex) arr = {0, 5, 9, 11, 8};
 	버블정렬 1회 : arr = {0, 5, 9, 8, 11};
@@ -110,7 +118,7 @@ void ImprovedBubbleSort(array<int, 10> & arr) {
 	이런 경우를 위해 bool 변수를 하나 만듦.
 	*/
 	bool flag = true;
-	array<int, 10> sorted_arr = arr;
+	array<int, ARRSIZE> sorted_arr = arr;
 	for (int i = 0; i < sorted_arr.size() - 1; i++) {
 		flag = true;
 		//두 개씩 비교해나가니까 맨 마지막 원소의 위치 -1 까지만 for 구문을 돌림.
@@ -135,7 +143,7 @@ void ImprovedBubbleSort(array<int, 10> & arr) {
 }
 
 //삽입 정렬. 배열의 원소들을 앞에서부터 비교하며 자신의 적절한 위치를 찾아 그곳에 자신을 삽입함.
-void InsertionSort(array<int, 10> & arr) {
+void InsertionSort(array<int, ARRSIZE> & arr) {
 	/*O(N^2) ~ O(N)
 	정렬하고자 하는 원소 하나를 필요한 경우 앞으로 땡겨오는 느낌.
 	ex) arr = {10, 20, 5, 30};
@@ -146,7 +154,7 @@ void InsertionSort(array<int, 10> & arr) {
 	3차전 : 정렬하고자 한흔 원소 = 30 
 		-> 30의 전 원소(20)와 30을 비교 -> 정렬할 필요 없음
 	*/
-	array<int, 10> sorted_arr = arr;
+	array<int, ARRSIZE> sorted_arr = arr;
 
 	//방법 1
 	/*int key = sorted_arr[1];
@@ -178,7 +186,7 @@ void InsertionSort(array<int, 10> & arr) {
 //합병 정렬. 폰 노이만이 고안한 정렬 알고리즘으로,
 //divide and conquer 방식이 적용된 분할 정복 알고리즘.
 //배열을 반으로 나눠서 각 부분 배열을 재귀적으로 정렬하고 하나로 합침.
-void MergeSort(array<int, 10> & arr) {
+void MergeSort(array<int, ARRSIZE> & arr) {
 	/*
 	O(nlogn) <-- 비교 nlogn + 이동 2nlogn == 3nlogn
 	특징 : 
@@ -199,7 +207,7 @@ void MergeSort(array<int, 10> & arr) {
 	}
 	return;
 }
-void RunMS(array <int, 10> & arr, int left, int right) {
+void RunMS(array <int, ARRSIZE> & arr, int left, int right) {
 	int mid;
 	//left와 right가 똑같은 상황 == divide가 끝나서 분할된 배열의 크기가 1인 상황
 	if (left < right) {
@@ -211,7 +219,7 @@ void RunMS(array <int, 10> & arr, int left, int right) {
 	}
 	return;
 }
-void Merge(array <int, 10> & arr, int left, int mid, int right) {
+void Merge(array <int, ARRSIZE> & arr, int left, int mid, int right) {
 	//임시 공간이 필요
 	array <int, 10> temp;
 	//최적화 하고 싶으면 (right - left) + 1의 크기만큼의 배열만 만들어도 됨.
@@ -252,7 +260,37 @@ void Merge(array <int, 10> & arr, int left, int mid, int right) {
 	return;
 }
 
-//퀵 정렬.
+//퀵 정렬. pivot을 이용한 정렬.
+void QuickSort(array<int, ARRSIZE> & arr, int left, int right) {
+	/*정렬할 data에 따라 실행 속도가 달라지는 불안정 정렬임.
+	divide and conquer 방식으로, 매우 빠르고 합병 정렬에 비해 비균등한 분할 방식임.
+	피봇을 기준으로 피봇보다 작은 값은 배열의 왼쪽으로, 큰 값은 배열의 오른쪽으로 보내며 피봇을 중심으로 정렬을 수행하고, 
+	정렬이 끝나면 왼쪽과 오른쪽 sub 배열에 대해 퀵정렬을 다시 시행한다.
+	*/
+
+	//base condition
+	if (left >= right) {
+		return;
+	}
+	
+	//피봇은 맨 끝에 값으로 설정하고,
+	//피봇 기준으로 swap 실행
+	int i = left; //교체해줄 인덱스 (교체해줄때만 1씩 증가)
+	int j = left; //비교해줄 인덱스 (계속 1씩 증가)
+	int pivot = arr[right];
+	while (j <= right) {
+		//피봇보다 작은 값 찾으면 교체, 큰 값 찾으면 비교할 위치반 증가
+		if (pivot >= arr[j]) {
+			swap(arr[i], arr[j]);
+			i++;
+		}
+		j++;
+	}
+	//왼쪽과 오른쪽 sub array에 대한 Quick Sort 실행
+	QuickSort(arr, left, i-2);
+	QuickSort(arr, i, right);
+}
+
 
 //힙 정렬.
 
