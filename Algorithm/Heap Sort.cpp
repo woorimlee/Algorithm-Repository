daee1976d94647215e1664c6b73ca38598ce6f14
woorimlee <and_lamyland@naver.com>
2020-04-20 23:38:17 +0900

    작성중

---
+++ Algorithm/Heap Sort.cpp
@@ -0,0 +1,61 @@
+/****************************************************************************
+힙 정렬 :
+	
+
+****************************************************************************/
+
+#include <iostream>
+#include <random>
+#include <array>
+#include <algorithm>
+
+#define ARRSIZE 10
+
+using namespace std;
+
+//난수 생성함수
+int GetRandomInt(int min, int max);
+void HeapSort(array<int, ARRSIZE>& arr);
+
+int main() {
+	array <int, ARRSIZE> arr = { 0 };
+	for (int i = 0; i < ARRSIZE; i++) {
+		arr[i] = GetRandomInt(-5, 5);
+	}
+	for (auto it : arr) {
+		cout << it << " ";
+	}
+	cout << "==> 정렬 전 배열\n======================================\n";
+	HeapSort(arr);
+	for (auto it : arr) {
+		cout << it << " ";
+	}
+	cout << "==> 정렬 후 배열\n";
+	return 0;
+}
+
+int GetRandomInt(int min, int max) {
+	random_device rd;
+	//랜덤 디바이스 객체 생성시 하드웨어의 리소스로 랜덤한 숫자를 생성
+	mt19937 mt(rd());
+	//메르센 트위스터 엔진에 객체 전달
+
+	//시드 설정 끝
+
+	//난수의 타입을 결정.
+	//min~ max까지 모든 숫자 다 나올 수 있음. 닫힌 구간임.
+	uniform_int_distribution <int> range(min, max);
+
+	//값 추출
+	return range(mt);
+}
+
+void HeapSort(array<int, ARRSIZE>& arr) {
+}
+
+
+//셸 정렬.
+
+//계수 정렬.
+
+//기수 정렬.
