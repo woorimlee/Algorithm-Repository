07bccbb31c07afe787b78e006587d4ad78087f79
woorimlee <and_lamyland@naver.com>
2020-05-17 23:37:34 +0900

    푸는중

--- BOJ/5430 AC (자료구조, 덱).cpp
+++ BOJ/5430 AC (자료구조, 덱).cpp
@@ -4,22 +4,16 @@
 
 using namespace std;
 
+bool r_flag = false;
 int T, n;
 string p, arr;
 deque <int> dq;
 
-void R(deque <int> & dq) {
-	if (dq.size() == 1) {
-		return;
-	}
-	int temp, j = dq.size()-1;
-	for (int i = 0; i < dq.size() / 2; i++) {
-		temp = dq[i];
-		dq[i] = dq[j];
-		dq[j] = temp;
-		j--;
-	}
-}
+//R 연산 : 
+//출력할 때 배열을 앞에서부터 출력하냐, 뒤에서부터 출력하냐만 결정해주면 됨.
+
+//D 연산 : 
+//pop_front & empty() 체크
 
 bool D(deque <int>& dq) {
 	if (dq.size() == 0) {
@@ -33,36 +27,43 @@ bool D(deque <int>& dq) {
 }
 
 bool AC() {
+	r_flag = false; //true면 R 한 거, false면 R 안한거 or 짝수번 한 거
+	int j = 1;
+	string snum = "";
 	dq.clear();
 	cin >> p; //함수
 	cin >> n;
 	cin >> arr; //배열
-	int j = 1;
-	int num;
-	for (int i = 0; i < arr.size(); i++) {
-		if ('0' <= arr[i] && arr[i] <= '9') {
-			string snum = "";
-			snum += arr[i];
-			while ('0' <= arr[i + j] && arr[i + j] <= '9' && i + j < arr.size()) {
-				snum += arr[i + j];
-				j++;
-				i++;
-			}
-			num = stoi(snum);
-			dq.push_back(num);
-			j = 1;
+
+	//입력받은 배열에서 숫자 쪼개 실제 배열에(덱) 집어넣기
+	for (auto it : arr) {
+		//숫자면 문자열에 더해두다가
+		//숫자 아닌순간 && 문자열에 어떤 값이 있다면
+		//배열에 추가하고 문자열 초기화
+		if ('0' <= it && it <= '9') {
+			snum += it;
+		}
+		else if(snum != "") {
+			dq.push_back(stoi(snum));
+			snum = "";
 		}
 	}
-	for (auto it : dq) {
-		cout << it << " ";
-	}
+
+	//함수 하나씩 확인하면서 덱에 적용하기
 	for (auto it : p) {
 		if (it == 'R') {
-			R(dq);
+			r_flag = !r_flag;
 		}
-		else {
-			if (!D(dq)) {
-				return false;	//에러 출력하면 더이상 뭐 안해도됨.
+		else { //함수가 D면
+			if (dq.size() == 0) {
+				cout << "error\n";
+				return false; //에러면 false를 리턴. 더이상 함수 사용 못하게
+			}
+			else if(r_flag) { // 거꾸로 배열이 구성되어 있으면
+				dq.pop_back();
+			}
+			else {
+				dq.pop_front();
 			}
 		}
 	}
@@ -73,24 +74,34 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	//R은 배열 숫자 순서 뒤집기
-	//D는 첫 수 버리기
-	//배열 비어있는데 D 쓰면 에러
 	
 	cin >> T;
 	while (T--) {
+		//AC를 실행하는데, error만 아니면 [배열값]출력
+		//AC는 error가 발생한 경우 false를 return.
 		if (AC()) {
-			if (dq.size() == 0) {
+			if (dq.empty()) {
 				cout << "[]\n";
 			}
-			else {
+			else { 
 				cout << "[";
-				for (int i = 0; i < dq.size() - 1; i++) {
-					cout << dq[i] << ",";
+				if (r_flag) {//R이 홀수 번 실행되면 거꾸로 출력
+					for (int i = 0; i < dq.size() - 1; i++) {
+						cout << dq.back() << ",";
+						dq.pop_back();
+					}
+					cout << dq[0];
 				}
-				cout << dq[dq.size() - 1];
+				else {
+					for (int i = 0; i < dq.size() - 1; i++) {
+						cout << dq[i] << ",";
+					}
+					cout << dq.back();
+				}
 				cout << "]\n";
 			}
+			
 		}
+
 	}
 }
