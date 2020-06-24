119629c74145aa48161c416893d204069eafb18f
woorimlee <and_lamyland@naver.com>
2020-06-24 20:26:20 +0900

    조금 바꿈

--- BOJ/11724 연결 요소의 개수.cpp
+++ BOJ/11724 연결 요소의 개수.cpp
@@ -10,19 +10,19 @@ vector <vector <int> > vec;
 void dfs(int node);
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	cin >> N >> M;
 	vec.resize(N + 1);
 	int nodeA, nodeB;
 	for (int i = 0; i < M; i++) {
 		cin >> nodeA >> nodeB;
-		//무방향 그래프를 만들어줌
 		vec[nodeA].push_back(nodeB);
 		vec[nodeB].push_back(nodeA);
 	}
 
 	for (int i = 1; i <= N; i++) {
-		//모든 노드를 기준으로 이미 방문한 노드가 아니라면 
-		//dfs 탐색 시작
+		//방문한 노드가 아니라면
 		if (!visited[i]) {
 			dfs(i);
 			ans++;
@@ -33,7 +33,6 @@ int main() {
 }
 
 void dfs(int node) {
-	//연결된 모든 노드를 dfs로 탐색할 것임
 	visited[node] = true;
 	for (int i = 0; i < vec[node].size(); i++) {
 		//방문한 노드가 아니라면
