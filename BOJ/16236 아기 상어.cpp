340f0e4a214a1b116a8cd00005ae377d79600a52
woorimlee <and_lamyland@naver.com>
2020-06-30 18:47:49 +0900

    푸는 중

--- BOJ/16236 아기 상어.cpp
+++ BOJ/16236 아기 상어.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+
 /*
 규칙 : 
 아기 상어는 자신의 크기보다 작거나 같은 곳으로 이동 가능
@@ -24,7 +25,8 @@
 3. 만약 먹은 물고기 갯수가 자신의 몸무게와 같으면 몸무게 1 증가 및 먹은 물고기 갯수 초기화
 4. 우선순위큐 초기화하면서 1 ~ 3 반복
 5. 더이상 먹을게 없으면 정답 출력
-
+다만, 물고기를 먹고 사이즈가 커지는 등의 특수 상황이 있으니
+먹는 것 먼저 처리하고 네 방향 이동하는 경우에 대해 q에 추가함.
 */
 
 using namespace std;
@@ -73,7 +75,36 @@ int main() {
 
 	return 0;
 }
-
 void bfs() {
+	priority_queue<Fish> pq;
+	pq.push({ s_x, s_y, 0 });
 
+	int x, y, dist;
+	Fish temp(0, 0, 0);
+	while (!pq.empty()) {
+		temp = pq.top();
+		pq.pop();
+		x = temp.x;
+		y = temp.y;
+		dist = temp.dist;
+		
+		//물고기 먹기
+		if (0 < map[y][x] && map[y][x] < s_size) {
+			//물고기 없애기, 먹은 갯수 증가 등의 작업
+			map[y][x] = 0;
+			ans += dist; //정답 갱신
+			dist = 0;
+			s_eat++; 
+			fill(visited[0][0], visited[21][21], false);
+			while (!pq.empty())
+				pq.pop();
+			//먹은 수랑 사이즈랑 같아지면
+			if (s_eat == s_size) {
+				s_eat = 0;
+				s_size++;
+			}
+		}
+
+		//이동시키기
+	}
 }
