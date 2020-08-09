b04f75b5deff54cc4da45ab22a04bfa03e168328
woorimlee <and_lamyland@naver.com>
2020-08-09 17:21:57 +0900

    잠시

--- BOJ/2606 virus.cpp
+++ BOJ/2606 virus.cpp
@@ -11,6 +11,8 @@ int N, S, answer = 0;
 bool visited[101] = { false, };
 vector <vector <int> > com_graph;
 
+int map[101][101];
+
 void dfs(int cur) {
 	visited[cur] = true;
 
@@ -24,19 +26,30 @@ void dfs(int cur) {
 	return;
 }
 
+void ddfs(int node) {
+	answer++;
+	visited[node] = true;
+	for (int i = 1; i <= N; i++) {
+		if (map[node][i] && !visited[i])
+			dfs(i);
+	}
+}
+
 int main(void) {
 	cin >> N >> S;
 
-	com_graph.resize(N+1);
+	com_graph.resize(N + 1);
 
 	int a, b;
 	while (S--) {
 		cin >> a >> b;
 		com_graph[a].push_back(b);
 		com_graph[b].push_back(a);
+		map[a][b] = map[b][a] = 1;
 	}
 
-	dfs(1);
+	//dfs(1);
+	ddfs(1);
 	cout << answer;
 
 	return 0;
