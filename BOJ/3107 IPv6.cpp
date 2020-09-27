d53bd4b2d7f07fd5edf1be6bcfeed7e690194b62
woorimlee <and_lamyland@naver.com>
2020-09-27 12:39:57 +0900

    푸는 중

---
+++ BOJ/3107 IPv6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string s;
+	vector <bool, string> IPv6;
+	IPv6.resize(8);
+	cin >> s;
+	int index = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if()
+	}
+
+
+}
