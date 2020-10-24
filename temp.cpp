985ebe06be56c1ce1727692a3cce415f971babf8
woorimlee <and_lamyland@naver.com>
2020-10-24 16:05:16 +0900

    temp

--- temp.cpp
+++ temp.cpp
@@ -1,29 +1,14 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <string>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-
-
 int main() {
 
-    //solution();
-    
-    string s = "abcde";
-    /*s.insert(1, "F"); //위치 무엇을
-    cout << s << "\n";
-    s.erase(1, 1); //위치 몇 개
-    cout << s << "\n";
-    s.erase(3);
-    cout << s << "\n";
-    s.replace(1, 1, "FGHI"); //위치 몇 개 무엇을
-    cout << s << "\n";
-    */
-    s.erase(s.find('b'), 1);
-    cout << s;
 
-
-    
 	return 0;
 }
