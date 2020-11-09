f9cc7d682eee2c9170aa55304ca47373b2e75b37
woorimlee <and_lamyland@naver.com>
2020-11-09 13:50:55 +0900

    class로 바꿔 만드는중

--- Data Structure/Min Heap.cpp
+++ Data Structure/Min Heap.cpp
@@ -9,90 +9,69 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
-int solution(vector<vector<string>> clothes) {
-	int answer = 1;
-	map<string, int> Map;
+class MinHeap {
+	int* heap = NULL; //heap elements array를 가리킬 공간
+	int capacity = 0; //maximum size
+	int heap_size = 0; //현재 heap의 size
 
-	for (int i = 0; i < clothes.size(); i++) {
+public :
+	MinHeap(int capa);
+	int parent(int i) { return (i - 1) / 2; }
+	int left(int i) { return 2 * i + 1; }
+	int right(int i) { return 2 * i + 2; }
 
-		//auto iter = Map.find(clothes[i][1]);
-
-		if (Map.find(clothes[i][1]) == Map.end()) {
-			Map[clothes[i][1]] = 1;
-		}
-		else {
-			Map[clothes[i][1]]++;
-		}
-	}
-
-	for (auto it : Map) {
-		cout << it.first << " " << it.second << "\n";
-	}
-
-	for (auto i = Map.begin(); i != Map.end(); i++) {
-		int value = i->second;
-		answer *= (value + 1);
-	}
-
-	return answer - 1;
-}
+	void insert_key(int key); // 새 값을 삽입. O(log n)
+	int get_min() { return heap[0]; } // 최솟값 return. O(1)
+	void pop_min(); // 최솟값 제거. O(log n)
+	void min_heapify(int i); //i 번째 노드의 subtree를 heapify
+	void decrease_key(int i, int new_key); // 특정 노드의 key 값을 수정한다. O(log n)
+	void delete_node(int i); // i 번째 index에 있는 노드 삭제
+	void print_heap();
+};
 
 int main() {
-	vector<vector<string>> clothes = { {"ye", "head"}, {"bl", "eye"}, {"gr", "head"} };
-	cout << solution(clothes);
+	MinHeap heap(10);
+	heap.insert_key(10);
+	heap.insert_key(2);
+	heap.insert_key(6);
+	heap.insert_key(8);
+	heap.insert_key(1);
+	heap.insert_key(5);
+	heap.insert_key(16);
+	heap.insert_key(4);
+	heap.print_heap();
 	return 0;
 }
 
-/*
-vector <int> min_heap;
-int heap_capacity = 10;
-
-int getMin(); // 최솟값 return. O(1)
-int popMin(); // 최솟값 제거. O(lgn)
-int decreaseKey(); //특정 노드의 key 값을 수정한다. O(1) ~ O(lgn)
-void insertKey(int key); //새 값을 삽입. O(lgn)
-int deleteKey(); //특정 노드 삭제. O(lgn)
-void print_min_heap();
-
-int main() {
-	insertKey(5);
-	insertKey(4);
-	insertKey(9);
-	insertKey(16);
-	insertKey(2);
-	insertKey(5);
-	insertKey(1);
-	
-	print_min_heap();
-
-	return 0;
+MinHeap::MinHeap(int capa) {
+	heap = new int[capa];
+	capacity = capa;
+	heap_size = 0;
 }
 
-void insertKey(int key) {
-	if (min_heap.size() == heap_capacity) {
+void MinHeap::insert_key(int k) {
+	if (heap_size == capacity) {
+		cout << "\nheap이 꽉 차있음.\n";
 		return;
 	}
-	//일단 집어넣고,
-	//부모와 자식간의 크기 비교만하면 된다.
-	min_heap.push_back(key);
-	int child_idx = min_heap.size() - 1;
-	int parent_idx = (child_idx - 1) / 2;
-	
-	while (parent_idx >= 0 && min_heap[child_idx] < min_heap[parent_idx]) {
-		swap(min_heap[child_idx], min_heap[parent_idx]);
-		child_idx = parent_idx;
-		parent_idx = (child_idx - 1) / 2;
+
+	heap_size++;
+	int i = heap_size - 1;
+	heap[i] = k;
+
+	while (i != 0 && heap[MinHeap::parent(i)] > heap[i]) {
+		swap(heap[MinHeap::parent(i)], heap[i]);
+		i = parent(i);
 	}
 }
 
-void print_min_heap() {
-	for (auto it : min_heap)
-		cout << it << " ";
+void MinHeap::print_heap() {
+	cout << "\nheap의 모든 노드 출력(인덱스 순서) :\n";
+	for (int i = 0; i < heap_size; i++)
+		cout << heap[i] << " ";
 	cout << "\n";
-}*/
+}
