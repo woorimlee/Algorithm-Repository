dea940c3de595d5bca0514a3f44333745c8d9095
woorimlee <and_lamyland@naver.com>
2020-12-03 20:15:23 +0900

    'update'

--- temp.cpp
+++ temp.cpp
@@ -7,8 +7,24 @@
 
 using namespace std;
 
-int main() {
 
+void permutation(string str, string prefix) {
+    if (str.length() == 0) {
+        cout << prefix << "\n";
+    }
+    else {
+        for (int i = 0; i < str.length(); i++) {
+            string rem = str.substr(0, i) + str.substr(i + 1);
+            permutation(rem, prefix + str[i]);
+        }
+    }
+}
+void permutation(string str) {
+    permutation(str, "");
+}
+
+int main() {
+    permutation("abcd");
 
 	return 0;
 }
--- 정보처리기사/정보처리기사 오답노트.cpp
+++ 정보처리기사/정보처리기사 오답노트.cpp
@@ -2,18 +2,18 @@
 
 int main()
 {
-	chapter1::section3();
-	chapter1::section4();
-	chapter1::section6();
-	chapter1::section8();
-	chapter1::cp("1장 프로그래밍 언어 활용 종합 문제\n");
-	chapter2::cp("2장 요구사항 확인 종합 문제\n");
-	chapter3::cp("3장 데이터 입, 출력 구현 종합 문제\n");
-	chapter4::section40();
+	//chapter1::section3();
+	//chapter1::section4();
+	//chapter1::section6();
+	//chapter1::section8();
+	//chapter1::cp("1장 프로그래밍 언어 활용 종합 문제\n");
+	//chapter2::cp("2장 요구사항 확인 종합 문제\n");
+	//chapter3::cp("3장 데이터 입, 출력 구현 종합 문제\n");
+	/*chapter4::section40();
 	chapter4::section42();
 	chapter4::section43();
 	chapter4::section45_46();
-	chapter4::cp("4장 통합 구현 종합 문제\n");
+	chapter4::cp("4장 통합 구현 종합 문제\n");*/
 	//chapter5::section48();
 	//chapter5::section51();
 	//chapter5::section52();
@@ -30,7 +30,7 @@ int main()
 	//chapter7::section68("68. 테스트 자동화 도구\n");
 	//chapter7::section69("69. 결함 관리\n");
 	//chapter7::section70("70. 어플리케이션 성능 분석\n");
-	//chapter8::cp("71 ~ 81. SQL 명령어 종합 문제\n");
+	chapter8::cp("71 ~ 81. SQL 명령어 종합 문제\n");
 	//chapter9::section82("82. 소프트웨어 개발 보안\n");
 	//chapter9::section84("84. 세션 통제");
 	//chapter9::section85_to_91("85 ~ 91. 오류 종류");
