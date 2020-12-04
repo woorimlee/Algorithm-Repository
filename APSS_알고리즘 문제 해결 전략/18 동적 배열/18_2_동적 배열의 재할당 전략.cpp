419a11e7c0776f626c591f8cf24e49a87e09b8a6
woorimlee <and_lamyland@naver.com>
2020-12-04 18:25:01 +0900

    후... struct는 heap 세그먼트 초과해도 그냥 할당을 해주는건가 후...

--- APSS_알고리즘 문제 해결 전략/18 동적 배열/18_2_동적 배열의 재할당 전략.cpp
+++ APSS_알고리즘 문제 해결 전략/18 동적 배열/18_2_동적 배열의 재할당 전략.cpp
@@ -14,12 +14,77 @@
 
 using namespace std;
 
-int main() {
+//데이터를 저장할 형식
+template <typename T>
+struct NODE {
+	T data;
+	NODE <T>* next;
+	NODE() { data = NULL; next = nullptr; }
+	NODE(const T& d, NODE<T>* ptr) {
+		data = d;
+		next = ptr;
+	}
+};
 
+//구조체 NODE가 저장된 배열을 순회하기 위한 iterator
+template <typename T>
+class _myiterator {
+	NODE <T>* cur;
+public :
+	_myiterator(NODE <T>* ptr = nullptr) { cur = ptr; }
+	_myiterator& operator++() {
+		cur = cur->next;
+		return *this;
+	}
+	_myiterator& operator+(int pos) { return *(this + pos); }
+	T& operator*() { return cur->data; }
+	bool operator==(const _myiterator& it) { return cur == it.cur; }
+	bool operator!=(const _myiterator& it) { return cur != it.cur; }
+};
+
+template <typename T>
+class myvector {
+public:
+	NODE <T>* head;
+	typedef _myiterator<T> iterator;
+	int max_size = 4;
+	int cur_size = 0;
+	iterator first;
+	iterator last;
+	myvector() { head = new NODE<T>[max_size]; }
+	bool append(T val) {
+		//push_back();
+		//reserve가 실행되면 false, 아니면 true를 return
+
+		//저장할 곳이 없다면 늘려
+		bool flag = true;
+		if (max_size == cur_size) {
+			//myvector<T>::reserve();
+			flag = false;
+		}
+		//맨 뒤에 값을 추가해
+		head[cur_size] = NODE<T>(val, nullptr);
+		if (cur_size != 0)
+			head[cur_size - 1].next = &head[cur_size];
+		cur_size++;
+		return flag;
+	}
+		
+	void size() { return size; }
+	void reserve(); //배열 크기 새로 할당, 옮기기, 기존거 삭제
+	void umove(); //옮기기
+	void change_array(); //orphan_all 실행, 속성 값 바꾸기
+	void orphan_all(); //모든 iterator가 nullptr을 가리킴
+	iterator begin() { return iterator(head); }
+	iterator end() { return iterator(nullptr); }
+};
+
+int main() {
+	/*
 	vector <int> vec;
 	int n = 10;
 	vec.reserve(n);
-	/* reserve(n) : 
+	reserve(n) : 
 	If n is greater than the current vector capacity, the function causes the container to reallocate its storage increasing its capacity to n (or greater).
 	In all other cases, the function call does not cause a reallocation and the vector capacity is not affected.
 	
@@ -31,13 +96,25 @@ int main() {
     }
 
 	_Newcapacity : n
-	capacity() : 
-	max_size() : 
-	_Xlength() : 
-	_Reallocate_exactly() : 
-
+	capacity() : _My_data._Myend - _My_data._Myfirst
+	max_size() : min(static_cast<size_type>((numeric_limits<difference_type>::max)()), _Alty_traits::max_size(_Getal())));
+		_Alty_traits ->  allocator_traits 여기의 max_size : Maximum size possible to allocate. _Getal() : get_allocator
+		-> 최대 numeric_limits 값과 allocator 값 비교
+	_Xlength() : _Xlength_error("vector too long");
+	_Reallocate_exactly() : set capacity to _Newcapacity (without geometric growth), provide strong guarantee : guarantee는 exception safety를 말하네.
+		orphan all iterators, discard old array, acquire new array
+	
+	결국 그냥 n 사이즈만큼 reallocate가 끝이네 (현재 크기가 n보다 작으면)
 	*/
 
+	myvector<int> myvec;
+	myvec.append(1);
+	myvec.append(3);
+	myvec.append(5);
+	myvec.append(7);
+	myvector<int>::iterator it;
+	for (it = myvec.begin(); it != myvec.end(); ++it)
+		cout << *it << "\n";
 
 	return 0;
 }
